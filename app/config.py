083fe0f4004e5e708616be4c84a3a37abfb67590
Maria Eduarda Chiquezi <seuemail@dominio.com>
2024-12-06 15:34:10 -0300

    fix(config): corrigir erro de configuração do banco de dados

--- app/config.py
+++ app/config.py
@@ -1,11 +1,20 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()  # Carregar as variáveis de ambiente do arquivo .env
 
 
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
-        self.app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+
+        # Certifique-se de que DATABASE_URL foi configurado corretamente
+        db_uri = os.getenv(
+            "DATABASE_URL", "sqlite:///default.db"
+        )  # Valor padrão em caso de falha
+
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
         self.app.config["PROPAGATE_EXCEPTIONS"] = True
         self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,93 +1,159 @@
-# user_service.py
-from sqlite3 import IntegrityError
-import sqlite3
-from flask import app, jsonify
+from flask import current_app
+from sqlalchemy.exc import SQLAlchemyError
+from app.exceptions.user_exceptions import (
+    ValidationError,
+    UniqueConstraintViolationError,
+)
 from app.repositories.user_repository import UserRepository
-from app.services.validate_user_data import ValidatorService
+from app.services.cpf_validator import CPFValidator
+from app.services.user_validator import ValidatorService
 from db import db
 from app.models.user_model import UserModel
-from flask import current_app
 
 
 class UserService:
     def __init__(self):
         self.repository = UserRepository()
 
-    from flask import current_app
-
-    def criar_usuario(self, data):
+    def create_user(self, data):
         try:
-            # Criação do usuário
-            user = UserModel(**data)
-            user.id = ValidatorService.generate_unique_id(user.cpf)  # Gera o ID
-            data["id"] = user.id
-
-            # Valida os dados do usuário antes de fazer a inserção
-            password_hash, validad = ValidatorService.validate_user_data(
-                user.email, user.password_hash, user.cpf
+            # Validar dados do usuário (email, CPF, senha e idade)
+            password_hash, formatted_cpf, valid = ValidatorService.validate_user_data(
+                data["email"], data["password"], data["cpf"], data["age"]
             )
-            data["password_hash"] = password_hash
+            if not valid:
+                raise ValidationError("Dados do usuário inválidos.")
 
-            if validad:
-                # Chamada para o repositório
-                query = self.repository.criar_usuario()
-                self.repository.save_to_db(query, data)
+            # Criar o usuário
+            user = UserModel(**data)
+            user.id = ValidatorService.generate_unique_id(
+                user.cpf
+            )  # Gerar ID único baseado no CPF
+            data["id"] = user.id
+            data["password_hash"] = password_hash
+            data["cpf"] = formatted_cpf
+
+            # Chamar o repositório para salvar o usuário no banco de dados
+            query = self.repository.create_user()
+            self.repository.save_to_db(query, data)
+
+            return {
+                "message": "Usuário criado com sucesso!"
+            }, 201  # Resposta de sucesso com status 201
+
+        except SQLAlchemyError as e:
+            db.session.rollback()  # Garantir que a transação seja revertida
+            error_str = str(e.__cause__)  # Obter a mensagem de erro original
+
+            # Identificar tipo de erro específico e criar uma mensagem mais detalhada
+            if "UNIQUE constraint failed" in error_str:
+                if "Users.email" in error_str:
+                    message = "O email já está registrado."
+                elif "Users.cpf" in error_str:
+                    message = "O CPF já está registrado."
+                else:
+                    message = "CPF ou Email já estão registrados."
                 return {
-                    "message": "Usuário criado com sucesso!"
-                }, 201  # Retorna dicionário e status
+                    "error": message
+                }, 400  # Retornar erro com status 400 (solicitação inválida)
+
+            # Registrar qualquer erro inesperado
+            current_app.logger.error(f"Erro inesperado: {str(e)}")
+            return {
+                "error": "Erro desconhecido no banco de dados."
+            }, 500  # Erro genérico de banco de dados
+
+        except ValidationError as e:
+            return {"error": str(e)}, 400  # Resposta de erro de validação
 
         except Exception as e:
-            db.session.rollback()
-            if "UNIQUE constraint failed" in str(e.orig):
-                return {
-                    "error": "CPF ou Email já cadastrado. Por favor, tente novamente."
-                }, 400  # Retorna erro de validação de chave única
-
-            # Usando current_app.logger para logar o erro inesperado
+            db.session.rollback()  # Reverter para garantir integridade
             current_app.logger.error(f"Erro inesperado: {str(e)}")
             return {
                 "error": f"Ocorreu um erro inesperado: {str(e)}"
-            }, 500  # Retorna erro genérico
+            }, 500  # Resposta genérica de erro
 
-    def listar_usuarios(self):
+    def list_users(self):
         try:
-            query = self.repository.listar_usuarios()
+            query = self.repository.list_users()
             result = db.session.execute(query).fetchall()
-            # Converte o resultado para dicionário
-            usuarios = [dict(row._mapping) for row in result]
-            return usuarios, 200
-        except Exception as e:
-            return {"error": str(e)}, 400
+            # Converter o resultado para um dicionário
+            users = [dict(row._mapping) for row in result]
 
-    def obter_usuario(self, id):
+            # Formatar CPF antes de retornar
+            for user in users:
+                user["cpf"] = CPFValidator.format_cpf(user["cpf"])
+
+            return users, 200  # Retornar lista de usuários com status 200
+
+        except Exception as e:
+            return {
+                "error": str(e)
+            }, 400  # Retornar resposta de erro caso a consulta ao banco falhe
+
+    def get_user(self, id):
         try:
-            query = self.repository.obter_usuario()
+            query = self.repository.get_user()
             result = db.session.execute(query, {"id": id}).fetchone()
             if result:
-                return dict(result._mapping), 200  # Converte Row para dict
-            return {"error": "Usuário não encontrado"}, 404
-        except Exception as e:
-            return {"error": str(e)}, 400
+                user = dict(result._mapping)
+                # Formatar CPF antes de retornar
+                user["cpf"] = CPFValidator.format_cpf(user["cpf"])
+                return user, 200  # Converter Row para dict e retornar com status 200
+            return {
+                "error": "Usuário não encontrado"
+            }, 404  # Retornar erro se o usuário não for encontrado
 
-    def atualizar_usuario(self, id, data):
+        except Exception as e:
+            return {
+                "error": str(e)
+            }, 400  # Retornar resposta de erro caso a consulta ao banco falhe
+
+    def update_user(self, id, data):
         try:
-            query = self.repository.atualizar_usuario()
-            data["id"] = id
-            validad = ValidatorService.validate_user_data(
-                data["email"], data["password_hash"], data["cpf"]
+            # Validar dados do usuário (email, CPF, senha e idade)
+            password_hash, cpf, valid = ValidatorService.validate_user_data(
+                data["email"], data["password"], data["cpf"], data["age"]
             )
-            if validad:
-                self.repository.save_to_db(query, data)
-                return {"message": "Usuário atualizado com sucesso!"}, 200
-        except Exception as e:
-            db.session.rollback()
-            return {"error": str(e)}, 400
+            if not valid:
+                raise ValidationError("Dados do usuário inválidos.")
 
-    def deletar_usuario(self, id):
-        try:
-            query = self.repository.deletar_usuario()
-            self.repository.save_to_db(query, {"id": id})
-            return {"message": "Usuário deletado com sucesso!"}, 200
+            # Atualizar dados do usuário
+            query = self.repository.update_user()
+            data["id"] = id
+            data["password_hash"] = password_hash
+            data["cpf"] = cpf
+
+            # Salvar dados atualizados no banco de dados
+            self.repository.save_to_db(query, data)
+
+            return {
+                "message": "Usuário atualizado com sucesso!"
+            }, 200  # Resposta de sucesso com status 200
+
+        except ValidationError as e:
+            return {"error": str(e)}, 400  # Resposta de erro de validação
+
         except Exception as e:
-            db.session.rollback()
-            return {"error": str(e)}, 400
+            db.session.rollback()  # Reverter para garantir integridade
+            return {"error": str(e)}, 400  # Resposta de erro se a atualização falhar
+
+    def delete_user(self, id):
+        try:
+            # Tentar excluir o usuário
+            query = self.repository.delete_user()
+            result = self.repository.save_to_db(query, {"id": id})
+
+            # Se nenhuma linha foi afetada, retornar 404 (Não encontrado)
+            if not result:
+                return {"error": "Usuário não encontrado"}, 404
+
+            return {
+                "message": "Usuário excluído com sucesso!"
+            }, 200  # Resposta de sucesso com status 200
+
+        except Exception as e:
+            db.session.rollback()  # Reverter para garantir integridade
+            return {
+                "error": str(e)
+            }, 400  # Retornar resposta de erro se a exclusão falhar
